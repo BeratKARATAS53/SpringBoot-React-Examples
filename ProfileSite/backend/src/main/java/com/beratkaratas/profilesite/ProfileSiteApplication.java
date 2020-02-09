147502d1903918aa1db53f6c60357cd3c30d94f5
OkCat <48531104+BeratKARATAS53@users.noreply.github.com>
2020-02-09 18:11:50 +0300

    Project Started

---
+++ ProfileSite/backend/src/main/java/com/beratkaratas/profilesite/ProfileSiteApplication.java
@@ -0,0 +1,13 @@
+package com.beratkaratas.profilesite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfileSiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProfileSiteApplication.class, args);
+	}
+
+}
---
+++ ProfileSite/backend/src/test/java/com/beratkaratas/profilesite/ProfileSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beratkaratas.profilesite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfileSiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
